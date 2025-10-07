bd845469c68f22b792459182a44fca0b18e99484
JakeCreate <developjake@gmail.com>
2025-10-07 11:02:12 -0700

    setup main.py format & output folder

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def generate_image(X, route, file_name, distance):
+    dpi = 100
+    fig, ax = plt.subplots(figsize=(1980/dpi, 1980/dpi), dpi=dpi)
+    sort_mask = np.array(route) - 1
+    X_sorted = X[sort_mask]
+    x, y = X_sorted[:, 0], X_sorted[:, 1]
+    
+    ax.scatter(x, y)
+    ax.plot(x, y)
+    
+    for i, (x_i, y_i) in enumerate(X):
+        ax.annotate(f'{i+1}', xy=(x_i, y_i), color='black', va='bottom', ha='left', fontsize='small')
+    
+    fig.savefig(f'../output/{file_name}_SOLUTION_{distance}.png', dpi=dpi)
+    
+
+if __name__ == '__main__':
+    # INPUT I
+    FOLDER_PATH = '../data/'
+    file_name = 'circle'
+    
+    X = np.loadtxt(FOLDER_PATH+file_name+'.txt')
+    
+    # START tsp anytime algorithm + input II: interruption key ENTER
+    
+    # END algorithm when key ENTER is pressed
+    distance, route = (1234, [1, 2, 3, 4]) # algorithm output
+    
+    # OUTPUT
+    if distance > 6000:
+        print(f'Warning: Solution is {math.ceil(distance)}, greater than the 6000 - meter constraint')
+    
+    print(f'total distance: {math.ceil(distance)}')
+    
+    route.append(route[0])
+    np.savetxt(f'../output/{file_name}_SOLUTION_{distance}.txt', route, fmt='%.0f')
+    
+    generate_image(X, route, file_name, distance)
+    
+    
